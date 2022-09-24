daaf148c4a0ed59e589ad7480bc22d30bb8da899
richard may clarkson <bobg2488@gmail.com>
2022-09-24 12:26:06 -0700

    get rid of a leftover thing from Why?

--- __ENGINE__/Shared/Sprites.cs
+++ __ENGINE__/Shared/Sprites.cs
@@ -1,14 +1,7 @@
 ﻿namespace __ENGINE__.Shared
 {
-    public class Sprites
+    public static class Sprites
     {
-        public static readonly Sprite grass_side = new(11, 11, 8, 8);
-        public static readonly Sprite grass_top = new(11, 1, 8, 8);
-        public static readonly Sprite dirt = new(1, 1, 8, 8);
-        public static readonly Sprite sand = new(21, 1, 8, 8);
-        public static readonly Sprite rectangle = new(31, 1, 8, 8);
-        public static readonly Sprite player = new(41, 1, 2, 12);
-        public static readonly Sprite player_top = new(45, 1, 2, 2);
-        public static readonly Sprite player_head = new(45, 5, 2, 2);
+
     }
 }
